b1a0f3092c84ab7e7151b5f38f13a1809713327f
selim <salimmia3745@gmail.com>
2023-07-22 18:51:02 +0600

    Conditional Rendering With && operator (47)

--- src/index.js
+++ src/index.js
@@ -66,14 +66,20 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  //   const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
@@ -103,7 +109,14 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come to visit us or order online.{" "}
+          </p>
+          <button className="btn">Order now</button>
+        </div>
+      )}
     </footer>
   );
 
