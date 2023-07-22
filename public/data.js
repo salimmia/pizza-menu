69ec7cb43493c4766c37173afdc1090bdc662e50
selim <salimmia3745@gmail.com>
2023-07-22 16:08:37 +0600

    Creating And Reusing a Component (36)

--- public/data.js
+++ public/data.js
@@ -1,6 +1,3 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,36 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <h1>Hello React!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
-
-// react v18
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-// react before v18
-// React.render(<App />);
